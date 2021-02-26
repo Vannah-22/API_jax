d9b0e8899e5e3fd06ec05592c315b1b831f63dc1
Vannah-22 <vannjackson16@gmail.com>
2021-02-26 16:15:20 -0700

    correct answer

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-// get the axios package and store it in variable
-let axios = require("axios");
-// the console RIGHT NOW is the GIT Bash terminal
-console.log(axios);
+// // get the axios package and store it in variable
+// let axios = require("axios");
+// // the console RIGHT NOW is the GIT Bash terminal
+// console.log(axios);
 
-// axios lets us make HTTP requests.
+// // axios lets us make HTTP requests.
 
-// get a list of Studio Ghibli films
-// make a variable to store the url of the API (endpoint)
-let url = "https://ghibliapi.herokuapp.com/people";
-/* let url = "https://ghibliapi.herokuapp.com/films"; ALL FILMS; */
-/* let url = "https://ghibliapi.herokuapp.com/locations"; LOCATIONS:*/
-/* let url = "https://ghibliapi.herokuapp.com/films/2de9426b-914a-4a06-a3a0-5e6d9d3886f6"; SPECIFIC
-MOVIE AND ITS INFO "The Secret World of Arriety" */
-/* let url = "https://ghibliapi.herokuapp.com/locations/469b14bd-5565-4436-bbed-c2036f42cc99"; SPECIFIC
-LOCATION AND AN ID OF A LOCATION "Himawari Nursery School" */
+// // get a list of Studio Ghibli films
+// // make a variable to store the url of the API (endpoint)
+// let url = "https://ghibliapi.herokuapp.com/people";
+// /* let url = "https://ghibliapi.herokuapp.com/films"; ALL FILMS; */
+// /* let url = "https://ghibliapi.herokuapp.com/locations"; LOCATIONS:*/
+// /* let url = "https://ghibliapi.herokuapp.com/films/2de9426b-914a-4a06-a3a0-5e6d9d3886f6"; SPECIFIC
+// MOVIE AND ITS INFO "The Secret World of Arriety" */
+// /* let url = "https://ghibliapi.herokuapp.com/locations/469b14bd-5565-4436-bbed-c2036f42cc99"; SPECIFIC
+// LOCATION AND AN ID OF A LOCATION "Himawari Nursery School" */
 
 
 
-// make a GET request to the url
-axios.get(url) /* AFTER the GET request is made, you recieve a response object */
-    .then(function(response) { // look at the response provided
-       // console.log(response.data); // all the data, the url specifies the part of the data //
-       for (let i = 0; i < data.length; i++) {
-           console.log(data[i].name);
-       }
-})        
+// // make a GET request to the url
+// axios.get(url) /* AFTER the GET request is made, you recieve a response object */
+//     .then(function(response) { // look at the response provided
+//        // console.log(response.data); // all the data, the url specifies the part of the data //
+//        for (let i = 0; i < data.length; i++) {
+//            console.log(data[i].name);
+//        }
+// })        
 
 // https://dog.ceo/dog-api/
 // - Use this API along with axios to get the url to a random image for an Otterhound
@@ -39,5 +39,5 @@ console.log(axios);
 let url = "https://dog.ceo/api/breed/Otterhound/images/random";
 axios.get(url)
     .then(function(response){
-        console.log(url);
+        console.log(response.data.message);
     })
